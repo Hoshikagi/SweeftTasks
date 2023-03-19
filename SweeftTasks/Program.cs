57d81bded7188448dd90c4f01aa75c8216d2ebc2
LukaChachanidze <luka.chachanidze.1@btu.edu.ge>
2023-03-19 17:07:11 +0400

    Add project files.

---
+++ SweeftTasks/Program.cs
@@ -0,0 +1,123 @@
+﻿
+
+#region tasks 
+static bool IsPalindrome(string text)
+{
+    string emptyString = "";
+    foreach (char c in text)
+    {
+        if (char.IsLetterOrDigit(c))
+        {
+            emptyString += c;
+        }
+    }
+
+    emptyString = emptyString.ToLower();
+
+    for (int i = 0; i < emptyString.Length / 2; i++)
+    {
+        if (emptyString[i] != emptyString[emptyString.Length - i - 1])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+// Example result for 80 would be [1 1 1 0 0] meaning that it's 1 (50tetri) 1(20tetri) 1(10tetri) 0 0
+static int[] MinSplit(int amount)
+{
+    int[] coins = { 50, 20, 10, 5, 1 };
+    int[] result = new int[coins.Length];
+    for (int i = 0; i < coins.Length; i++)
+    {
+        result[i] = amount / coins[i];
+        amount %= coins[i];
+    }
+    return result;
+}
+
+
+static int NotContains(int[] array)
+{
+    int n = array.Length;
+
+    int j = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (array[i] > 0)
+        {
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+            j++;
+        }
+    }
+
+    for (int i = 0; i < j; i++)
+    {
+        int index = Math.Abs(array[i]) - 1;
+        if (index < j && array[index] > 0)
+        {
+            array[index] = -array[index];
+        }
+    }
+
+    for (int i = 0; i < j; i++)
+    {
+        if (array[i] > 0)
+        {
+            return i + 1;
+        }
+    }
+
+    return j + 1;
+}
+
+static bool IsProperly(string sequence)
+{
+    int count = 0;
+
+    foreach (char c in sequence)
+    {
+        if (c == '(')
+        {
+            count++; 
+        }
+        else if (c == ')')
+        {
+            count--;
+        }
+
+        if (count < 0) 
+        {
+            return false;
+        }
+    }
+
+    return count == 0;
+}
+ static int CountVariants(int stairCount)
+{
+    if (stairCount <= 0)
+    {
+        return 0; 
+    }
+    else if (stairCount == 1)
+    {
+        return 1; 
+    }
+
+    int[] variants = new int[stairCount + 1]; 
+    variants[0] = 1;
+    variants[1] = 1; 
+
+    for (int i = 2; i <= stairCount; i++)
+    {
+        variants[i] = variants[i - 1] + variants[i - 2];
+    }
+
+    return variants[stairCount]; 
+}
+
+#endregion
